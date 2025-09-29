4072efd0af6e4b32defac3be7fa59ebfe07796c9
Zander3-2 <zaen0001@stud.ek.dk>
2025-09-29 14:54:45 +0200

    Atributter, konstuktør, getters, setters.

--- src/Marsvin.java
+++ src/Marsvin.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class Marsvin {
+    private String name;
+    private String race;
+    private int fodder;
+
+    public Marsvin(String name, String race, int foder) {
+        this.name = name;
+        this.race = race;
+        this.fodder = fodder;
+    }
+public String Getname(){return name;}
+public String Getrace(){return race;}
+public int Getfodder(){return fodder;}
+
+public void Setname(String name){this.name = name;}
+public void Setrace(String race){this.race = race;}
+public void Setfodder(int fodder){this.fodder = fodder;}
+
+    @Override
+    public String toString(){
+    return "";
+    }
 }
+
