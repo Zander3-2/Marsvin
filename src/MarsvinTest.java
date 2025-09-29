be99add777cad7a64dbb0f1677660973d9771eb3
MartinWagnerThomsen <marwagtho@gmail.com>
2025-09-29 15:04:53 +0200

    Commit01

--- src/MarsvinTest.java
+++ src/MarsvinTest.java
@@ -1,2 +1,24 @@
+package PACKAGE_NAME;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
 public class MarsvinTest {
+    public static void main(String[] args) {
+        ArrayList<Marsvin> Marsvin = new ArrayList<>();
+
+
+        public class ReadFile {
+            public static void main(String[] args) {
+                File Marsvin = new File("Marsvin.txt");
+
+                try (Scanner myReader = new Scanner(Marsvin)) {
+                    while (myReader.hasNextLine()) {
+                        String data = myReader.nextLine();
+                        System.out.println(data);
+
+    }
 }
